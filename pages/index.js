0376026acfe74a84e59359358d7598d9b5cdaa27
Will <wworland@gmail.com>
2020-10-28 21:49:21 -0500

    initial udpates

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,58 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title> blueboxb.us | home </title>{" "}
         <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+      </Head>{" "}
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
+          Welcome to <a href="#"> blueboxb.us! </a>{" "}
+        </h1>{" "}
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
+          follow along on our skoolie journey{" "}
+        </p>{" "}
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
+          <a href="/tardis" className={styles.card}>
+            <h3> Our T.A.R.D.I.S. & rarr; </h3>{" "}
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
+              {" "}
+              Time And Relative Dimension In Space(TARDIS) Where my whovians at
+              ? .{" "}
+            </p>{" "}
+          </a>{" "}
+          <a href="/companions" className={styles.card}>
+            <h3> Companions & rarr; </h3>{" "}
+            <p>
+              {" "}
+              2 Parents, 3 Kids, 2 Dogs, 1 Cat, 2 Geckos & 2 Snakes... in a bus.{" "}
+            </p>{" "}
+          </a>{" "}
+          <a href="/console" className={styles.card}>
+            <h3> Console & rarr; </h3> <p> Under construction. </p>{" "}
+          </a>{" "}
+          <a href="/blog" className={styles.card}>
+            <h3> Blog & rarr; </h3>{" "}
+            <p>
+              {" "}
+              Follow along on our crazy, wild ride through time and space!{" "}
+            </p>{" "}
+          </a>{" "}
+        </div>{" "}
+      </main>{" "}
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+          Powered by{" "}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />{" "}
+        </a>{" "}
+      </footer>{" "}
     </div>
-  )
+  );
 }
