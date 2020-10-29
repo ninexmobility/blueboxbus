d45467454b3e00d0092c6153b9079f5804df6d0b
Will <wworland@gmail.com>
2020-10-28 21:58:38 -0500

    initial udpates4

--- pages/index.js
+++ pages/index.js
@@ -45,6 +45,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          Copyright &copy; <a href="https://ninexmobility.com">ninexmobility</a>
           Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />{" "}
         </a>{" "}
